6cde55ef8a758bb142c55da8dde2b0a923bfb99b
makekop <72009584+makekop@users.noreply.github.com>
2025-12-17 15:30:03 +0200

    add totalcount for sunshine seconds. add converter function to readable time. return only pastdays or fo recast days or both.

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,21 +1,49 @@
 "use server";
-export async function getSunShineData() {
-    const latitude = 60.17136;
-    const longitude = 24.927353;
-    const timezone = "auto";
-    const daily = "sunshine_duration";
-    const past_days = 10;
+
+export async function getSunShineData(past_days = 0, forecast_days = 0) {
+    const dataObject = {
+        latitude: 60.17136,
+        longitude: 24.927353,
+        timezone: "auto",
+        daily: "sunshine_duration",
+        past_days: past_days,
+        forecast_days: forecast_days,
+    };
     try {
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=${daily}&timezone=${timezone}&past_days=${past_days}`;
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${dataObject.latitude}&longitude=${dataObject.longitude}&daily=${dataObject.daily}&timezone=${dataObject.timezone}&past_days=${dataObject.past_days}&forecast_days=${dataObject.forecast_days}`;
         const res = await fetch(url);
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         const data = await res.json();
 
-        return data.daily.time.map((date, i) => ({
+        const sunshineAndDateData = data.daily.time.map((date, i) => ({
             date,
-            sunshine: data.daily.sunshine_duration[i],
+            sunshine: data.daily.sunshine_duration[i] ?? 0,
         }));
+        const sumWithSunInitial = sunshineAndDateData.reduce(
+            (acc, item) => acc + item.sunshine,
+            0
+        );
+        function sunshineTotalReturn(sum) {
+            let h = Math.floor(sum / 3600);
+            let m = Math.floor((sum % 3600) / 60);
+            let s = Math.floor((sum % 3600) % 60);
+
+            const hDisplay =
+                h > 0 ? h + (h === 1 ? " hour, " : " hours, ") : "";
+            const mDisplay =
+                m > 0 ? m + (m === 1 ? " minute, " : " minutes, ") : "";
+            const sDisplay =
+                s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
+            return hDisplay + mDisplay + sDisplay;
+        }
+        const sunshineTotal = sunshineTotalReturn(sumWithSunInitial);
+
+        return {
+            daily: sunshineAndDateData,
+            total: sunshineTotal,
+        };
     } catch (err) {
         console.error("Error", err);
+        return [];
     }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,53 @@
+"use client";
+import { useState, useEffect } from "react";
 import { getSunShineData } from "./actions";
 
-const App = async () => {
-    const data = await getSunShineData();
+export default function App() {
+    const [pastDays, setPastDays] = useState(5);
+    const [forecastDays, setForecastDays] = useState(0);
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [total, setTotal] = useState("");
+
+    useEffect(() => {
+        async function load() {
+            setLoading(true);
+            try {
+                const result = await getSunShineData(pastDays, forecastDays);
+                setData(result.daily);
+                setTotal(result.total);
+            } catch (err) {
+                console.error("Error", err);
+            } finally {
+                setLoading(false);
+            }
+        }
+        load();
+    }, [pastDays, forecastDays, total]);
     return (
-        <div>
+        <div className="">
             <ul>
                 {data.map((item) => (
                     <li key={item.date}>
-                        {item.date}: {(item.sunshine / 3600).toFixed(2)} hours
+                        {item.date}:{" "}
+                        {item.sunshine / 3600 < 1
+                            ? (item.sunshine / 60).toFixed(2)
+                            : (item.sunshine / 3600).toFixed(2)}
+                        {item.sunshine / 3600 < 1 ? "minutes" : "hours"}
                     </li>
                 ))}
             </ul>
+            <div>
+                <button onClick={() => setPastDays(10)}>Get 10 pastDays</button>
+            </div>
+            <div>
+                {" "}
+                <button onClick={() => setForecastDays(10)}>
+                    Get 10 forecastDays
+                </button>
+            </div>
+            <div>{total}</div>
+            {loading && <p>Loading...</p>}
         </div>
     );
-};
-export default App;
+}
